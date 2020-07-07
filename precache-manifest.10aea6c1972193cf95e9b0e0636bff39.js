3932b133eb3abab7196b698815e4b93f1420e649
MadhusBucket <flewdemons@gmail.com>
2020-07-07 13:27:35 +0530

    Updates

---
+++ precache-manifest.10aea6c1972193cf95e9b0e0636bff39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc5feb0a27adc732c87fdc395f4c56a",
+    "url": "/Corona_Cases/index.html"
+  },
+  {
+    "revision": "9cd35d6624083a18b977",
+    "url": "/Corona_Cases/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "7116f65ad85c5118df83",
+    "url": "/Corona_Cases/static/js/2.fceb10c3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona_Cases/static/js/2.fceb10c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd35d6624083a18b977",
+    "url": "/Corona_Cases/static/js/main.98fb750f.chunk.js"
+  },
+  {
+    "revision": "78af78fb22f7365af448",
+    "url": "/Corona_Cases/static/js/runtime-main.b50c321a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona_Cases/static/media/image.d7265326.png"
+  }
+]);
